8f15b31283a14a587ae50a7d13a9625b9eb98dda
Michael <michaeldkim1@gmail.com>
2024-03-08 21:06:09 -0500

    Updated Map V1

--- src/components/Places.js
+++ src/components/Places.js
@@ -10,15 +10,29 @@ import {
 } from "@vis.gl/react-google-maps";
 
 export default function Places() {
-  const position = { lat: 53.54, lng: 10 };
+  const position = { lat: 40.730610, lng:-73.935242 };
+  const [open, setOpen] = useState(false)
 
   return (
     <APIProvider apiKey={process.env.REACT_APP_MAP_API_KEY}>
       <div className="h-dvh w-dvh">
-        <Map zoom={9} center={position} mapId={process.env.REACT_APP_MAP_ID}>
+        <Map zoom={10} center={position} mapId={process.env.REACT_APP_MAP_ID}>
+          <AdvancedMarker position={position} onClick={() => setOpen(true)}>
+            <Pin  
+              background={"green"}
+              borderColor={"black"}
+              glyphColor={"white"}
+            />
+          </AdvancedMarker>
 
-
-          
+          {open && (
+            <InfoWindow
+              position={position}
+              onCloseClick={() => setOpen(false)}
+            >
+              <div>You are here!</div>
+            </InfoWindow>
+          )}
         </Map>
       </div>
     </APIProvider>
