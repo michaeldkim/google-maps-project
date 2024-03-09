64ed6c9ff4a0dfdd28b00b2e52f7528dab87cfee
Michael <michaeldkim1@gmail.com>
2024-03-08 20:33:21 -0500

    got the map working

--- src/components/Places.js
+++ src/components/Places.js
@@ -1,71 +1,26 @@
-import { useState, useMemo } from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-import usePlacesAutocomplete, {
-  getGeocode,
-  getLatLng,
-} from "use-places-autocomplete";
+
+
+import { useState } from "react";
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+} from "@vis.gl/react-google-maps";
 
 export default function Places() {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_MAP_API_KEY,
-    libraries: ["places"],
-  });
-
-  if (!isLoaded) return <div>Loading...</div>;
-  return <Map />;
-}
-
-function Map() {
-  const center = useMemo(() => ({ lat: 43.45, lng: -80.49 }), []);
-  const [selected, setSelected] = useState(null);
+  const position = { lat: 53.54, lng: 10 };
 
   return (
-    <>
-      <div className="places-container">
-        <PlacesAutocomplete setSelected={setSelected} />
+    <APIProvider apiKey={process.env.REACT_APP_MAP_API_KEY}>
+      <div className="h-dvh w-dvh">
+        <Map zoom={9} center={position} mapId={process.env.REACT_APP_MAP_ID}>
+
+
+          
+        </Map>
       </div>
-
-      <GoogleMap
-        zoom={10}
-        center={center}
-        mapContainerClassName="map-container"
-      >
-        {selected && <Marker position={selected} />}
-      </GoogleMap>
-    </>
-  );
+    </APIProvider>
+  )
 }
-
-const PlacesAutocomplete = ({ setSelected }) => {
-  const {
-    ready,
-    value,
-    setValue,
-    suggestions: { status, data },
-    clearSuggestions,
-  } = usePlacesAutocomplete();
-
-  const handleSelect = async (address) => {
-    setValue(address, false);
-    clearSuggestions();
-
-    const results = await getGeocode({ address });
-    const { lat, lng } = await getLatLng(results[0]);
-    setSelected({ lat, lng });
-  };
-
-  return (
-    <div>
-      <input
-        value={value}
-        onChange={handleInput}
-        disabled={!ready}
-        placeholder="Search an address"
-        list="suggestions"
-      />
-      <datalist id="suggestions" onSelect={(e) => handleSelect(e.target.value)}>
-        {status === "OK" && renderSuggestions()}
-      </datalist>
-    </div>
-  );
-};
