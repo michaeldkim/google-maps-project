cec9ae3b1930697bc26ace6c0ddeea76f814bc4e
Michael <michaeldkim1@gmail.com>
2024-03-21 15:35:18 -0400

    Autoplaces complete started

--- src/components/Places.js
+++ src/components/Places.js
@@ -1,40 +1,74 @@
-
-
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import {
   APIProvider,
-  Map,
   AdvancedMarker,
+  Map,
   Pin,
   InfoWindow,
 } from "@vis.gl/react-google-maps";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { Combobox, Popover } from "@headlessui/react";
 
-export default function Places() {
-  const position = { lat: 40.730610, lng:-73.935242 };
+ export default function Places() {
+  const center = useMemo(() => ({ lat: 40.730610, lng:-73.935242 }), []);
+  const [selected, setSelected] = useState(null);
   const [open, setOpen] = useState(false)
-
+  
   return (
     <APIProvider apiKey={process.env.REACT_APP_MAP_API_KEY}>
       <div className="h-dvh w-dvh">
-        <Map zoom={10} center={position} mapId={process.env.REACT_APP_MAP_ID}>
-          <AdvancedMarker position={position} onClick={() => setOpen(true)}>
-            <Pin  
-              background={"green"}
-              borderColor={"black"}
-              glyphColor={"white"}
-            />
-          </AdvancedMarker>
+        <div className="places-container">
+          <PlacesAutocomplete setSelected={setSelected} />
+        </div>
 
-          {open && (
-            <InfoWindow
-              position={position}
-              onCloseClick={() => setOpen(false)}
-            >
-              <div>You are here!</div>
-            </InfoWindow>
-          )}
+        <Map zoom={10} center={center} mapId={process.env.REACT_APP_MAP_ID}>
+          { selected && <AdvancedMarker position={selected} />}
         </Map>
       </div>
     </APIProvider>
-  )
+  );
 }
+
+const PlacesAutocomplete = ({ setSelected }) => {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
+
+  const handleSelect = async (address) => {
+    setValue(address, false);
+    clearSuggestions();
+
+    const results = await getGeocode({ address });
+    const { lat, lng } = await getLatLng(results[0]);
+    setSelected({ lat, lng });
+    clearSuggestions();
+  };
+
+  return (
+    <Combobox onChange={handleSelect}>
+      <Combobox.Input 
+        value={value} 
+        onChange={(e) => setValue(e.target.value)}
+        disabled={!ready}
+        className="combobox-input"
+        placeholder="Search an address"
+      />
+      <Popover>
+        <Combobox.Options>
+          {status === "OK" &&
+            data.map(({ place_id, description }) => (
+              <Combobox.Option key={place_id} value={description} />
+            ))}
+        </Combobox.Options>
+      </Popover>
+    </Combobox>
+  );
+};
